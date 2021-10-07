f1b7c5bc994eb72dd7699447290bcf048904bfdc
Matias Almiron <matiturock@gmail.com>
2021-10-07 19:12:43 -0300

    ejercicio 5

---
+++ java_info/src/level1/Ejercicio5.java
@@ -0,0 +1,27 @@
+/**
+ * Se desea una aplicación que solicite 2 números enteros y realice la operación de multiplicación por sumas sucesivas (sin uso de librerías).
+ */
+
+package level1;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("ingresa dos números enteros:");
+    int num1 = s.nextInt();
+    int num2 = s.nextInt();
+
+    int acum = 0;
+    for (int i = 0; i < num2; i++) {
+      acum += num1;
+    }
+
+    System.out.println(String.format("%d x %d = %d", num1, num2, acum));
+
+    s.close();
+  }
+
+}
